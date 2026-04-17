181fca5897093e3aa48163ef281413ecb880bfa3
anshul9090 <anshul9090@github.com>
2026-04-17 17:35:10 +0530

    fix: set production API URL to Railway backend

--- GlobalTextSummarizerUI/src/environments/environment.production.ts
+++ GlobalTextSummarizerUI/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7266' // ← we will replace this after backend deploys
+   apiUrl: 'https://distill-backend-production.up.railway.app'// ← we will replace this after backend deploys
 };
